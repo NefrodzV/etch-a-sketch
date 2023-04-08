f30c86f2efe6a037fbbae0353a66a70b9a24a003
Neftaly <neftalyrv23@gmail.com>
2023-04-08 16:44:13 -0400

    Remove the existing grid nodes successful. Changing the area of the grid container class. Also creating new grids with the input given. NOTE: After 100 it goes ofscreen

--- javascript.js
+++ javascript.js
@@ -3,13 +3,15 @@ let gridNumber = 16;
 let grids = gridNumber ** 2;
 let gridArea;
 
+// Need this value to remove existing nodes
+let temp = 0;
 
 const gridContainer = document.querySelector('.grid-container');
 
-function createGrids(grids) {
+function createGrids() {
     
     console.log(`${(gridNumber)** 2}`)
-    for(let i = 0; i < grids; i++) {
+    for(let i = 0; i < gridNumber ** 2; i++) {
         console.log(i);
         const grid = document.createElement('div');
         grid.classList.add("grid"); 
@@ -20,25 +22,44 @@ function createGrids(grids) {
 }
 createGrids(grids);
 
+function removeGrids(number) {
+    for(let i = 0; i < number ** 2; i++) {
+        const gridToRemove = document.querySelector('.grid')
+        gridToRemove.remove();
+    }
+}
 function calculateDivArea() {
-
+    gridArea = (26 * gridNumber) + gridNumber;
 }
 const changeButton = document.querySelector('#change-grids-button');
+
 changeButton.addEventListener('click', () => {
-    let numberOfGrids = prompt("Enter the number of grids");
+    temp = gridNumber;
+    gridNumber = Number(prompt("Enter the number of grids"));
     // Checking if the values acceptable
-    if(numberOfGrids < 0) {
+    if(gridNumber < 0) {
         alert("Cannot be less than 0");
         return;
     }
-    if(!Number.isInteger(numberOfGrids)) {
+    if(!Number.isInteger(gridNumber)) {
         alert("Number has to be a integer");
         return;
     }
-    if(numberOfGrids > MAX_GRIDS) {
+    if(gridNumber > MAX_GRIDS) {
         alert("The max value of grid is 100");
         return;
     }
+
+    removeGrids(temp);
+    calculateDivArea();
+    // createGrids(numberOfGrids);
+    
+    // If everything is acceptable remove elements
+    
+    gridContainer.style.width = gridArea + "px";
+    gridContainer.style.height = gridArea + "px";
+
+    createGrids();
 });
 
 
