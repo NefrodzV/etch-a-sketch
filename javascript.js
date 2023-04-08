f2ebac020a4dca42092ec78860d294b05569a205
Neftaly <neftalyrv23@gmail.com>
2023-04-08 15:49:34 -0400

    Checking if the input value is less than 0, an integer and not more than max grid value.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
-let gridNumber = 16
-
+const MAX_GRIDS = 100;
+let gridNumber = 16;
 let grids = gridNumber ** 2;
+let gridArea;
 
 
 const gridContainer = document.querySelector('.grid-container');
@@ -12,10 +13,33 @@ function createGrids(grids) {
         console.log(i);
         const grid = document.createElement('div');
         grid.classList.add("grid"); 
-        // grid.textContent = i;
+        // grid.textContent = i+1;
         gridContainer.appendChild(grid);
     }
     
 }
-
 createGrids(grids);
+
+function calculateDivArea() {
+
+}
+const changeButton = document.querySelector('#change-grids-button');
+changeButton.addEventListener('click', () => {
+    let numberOfGrids = prompt("Enter the number of grids");
+    // Checking if the values acceptable
+    if(numberOfGrids < 0) {
+        alert("Cannot be less than 0");
+        return;
+    }
+    if(!Number.isInteger(numberOfGrids)) {
+        alert("Number has to be a integer");
+        return;
+    }
+    if(numberOfGrids > MAX_GRIDS) {
+        alert("The max value of grid is 100");
+        return;
+    }
+});
+
+
+
